2bd9a4f79c7d0418b739d5784dcb50bb46c21bfb
Shravani-Dake <shravanidake17@gmail.com>
2026-01-21 18:08:54 +0530

    functions in JS Uploaded

---
+++ Examples/21-01-2026/function.js
@@ -0,0 +1,25 @@
+// funtion : it is a block of code used to execute a specific task.
+/*
+definition : means writing the function and assigning it to a variable.
+             must be defined before use.
+*/
+//add(2, 3);   // Error: Cannot access 'add' before initialization
+const addition = function(a, b) {
+  return a + b;
+};
+
+//declaration : simply write a function with actual logic in scope.
+function function_name(){
+                         console.log("shravani");
+                        };
+function add(a, b){   
+  return a + b;
+                  };
+
+// to store  function in a variable 
+const variable_name = function function_name(){
+                                                console.log("shravani");
+                                               };
+//Array function :
+const name = () => console.log("Shravani");
+const adds = (a, b) => a + b;   // Arrow Function Definition
